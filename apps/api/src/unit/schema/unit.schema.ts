d3ad76b6e303706c83d01fd1f3cf376d842a9167
Abhishek Soni <abhisheksoni@codebuddy.co>
2024-08-08 17:07:20 +0530

    :sparkles: feat(api): created delete api to delete unit with testcase

--- apps/api/src/unit/schema/unit.schema.ts
+++ apps/api/src/unit/schema/unit.schema.ts
@@ -111,6 +111,9 @@ export class Unit extends Document {
     videoTourLink: string;
   };
 
+  @Prop({ type: Boolean, default: false })
+  isDeleted: boolean;
+
   @Prop({ type: Types.ObjectId, ref: 'User' })
   createdById: Types.ObjectId;
 
