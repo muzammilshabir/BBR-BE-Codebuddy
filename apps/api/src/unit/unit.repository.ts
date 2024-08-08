a353e2e3debb473bc4905ecf686fb30ba69cce36
Suman Sarkar <suman@codebuddy.co>
2024-08-08 10:41:44 +0530

    :bug: fix(api): removed unnecessary imports

--- apps/api/src/unit/unit.repository.ts
+++ apps/api/src/unit/unit.repository.ts
@@ -3,8 +3,6 @@ import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { Unit } from './schema/unit.schema';
 import { BaseRepository } from '@bbr/api-core/modules/db/base.repository';
-import { AddUnitKeyFeaturesDto } from './dto/unit-key-features.dto';
-import { AddVisualsDto } from './dto/add-visuals.dto';
 
 @Injectable()
 export class UnitRepository extends BaseRepository<Unit> {
