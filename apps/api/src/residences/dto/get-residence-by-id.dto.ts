0b02560e84fcf03dd414480d2207c42118c22500
Suman Sarkar <suman@codebuddy.co>
2024-08-13 14:40:59 +0530

    :bug: fix(api): dto updated

--- apps/api/src/residences/dto/get-residence-by-id.dto.ts
+++ apps/api/src/residences/dto/get-residence-by-id.dto.ts
@@ -5,7 +5,7 @@ import { joiObjectIdValidator } from '@bbr/api-core/modules/custome-validations/
 export class GetResidenceByIdDto {
   @ApiProperty({
     example: '64b1b5f4e05c12a1f5d8e7c2',
-    description: 'ID of the Unit',
+    description: 'ID of the Residence',
     required: true,
   })
   residenceId: string;
