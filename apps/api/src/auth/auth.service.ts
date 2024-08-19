0ae8a2908d9ea6af58abf3c77890fbfbcfd85766
Abhishek Soni <abhisheksoni@codebuddy.co>
2024-08-19 18:35:52 +0530

    :sparkles: feat(api): seller signup, verify and email login api created

--- apps/api/src/auth/auth.service.ts
+++ apps/api/src/auth/auth.service.ts
@@ -12,7 +12,7 @@ import { User } from '../users/schema/user.schema';
 import { UserService } from '../users/user.service';
 import { LoginDto } from './dto/login.dto';
 import { ResendVerificationEmailDto } from './dto/resendVerificationEmail';
-import { BuyerSignupDto } from './dto/signup.dto';
+import { BuyerSignupDto, SellerSignupDto } from './dto/signup.dto';
 import { UpdateBuyerProfileDto } from './dto/updateProfile';
 import { VerifyUserDto } from './dto/verifyUser.dto';
 import { JwtPayloadType } from './type/jwt-payload.type';
@@ -56,6 +56,14 @@ export class AuthService {
 
     await this.userService.verifyUserEmail(token, email, role);
 
+    if (role === UserRole.SELLER && user.acceptBBRCommitment !== true) {
+      return {
+        tokens: await this.generateJwtToken(user),
+        errorCode: ExceptionCodes.acceptBBRCommitment,
+        message: 'Please accept BBR commitment',
+      };
+    }
+
     return await this.generateJwtToken(user);
   }
 
@@ -83,6 +91,14 @@ export class AuthService {
     const isPasswordMatch = await argon.verify(user.password, loginDto.password);
 
     if (!isPasswordMatch) throw new ForbiddenException('Invalid credentials');
+
+    if (role === UserRole.SELLER && user.acceptBBRCommitment !== true) {
+      return {
+        tokens: await this.generateJwtToken(user),
+        errorCode: ExceptionCodes.acceptBBRCommitment,
+        message: 'Please accept BBR commitment',
+      };
+    }
 
     return { tokens: await this.generateJwtToken(user) };
   }
@@ -142,5 +158,23 @@ export class AuthService {
     if (loggedInUser.role !== UserRole.BUYER) throw new UnauthorizedException('Invalid token');
 
     return await this.userService.update(loggedInUser.sub, updateBuyerDto);
+  }
+
+  async signupDeveloper(sellerSignupDto: SellerSignupDto) {
+    const user = await this.userService.create({
+      fullName: sellerSignupDto.fullName,
+      email: sellerSignupDto.corporateEmail,
+      corporateEmail: sellerSignupDto.corporateEmail,
+      password: await argon.hash(sellerSignupDto.password),
+      signupMethod: SignupMethod.EMAIL,
+      role: UserRole.SELLER,
+      agreeToTerms: true,
+      receiveLuxuryInsights: sellerSignupDto.receiveLuxuryInsights,
+      acceptBBRCommitment: false,
+    });
+
+    this.sendVerificationEmail(user.email, user.verificationToken);
+
+    return user;
   }
 }
--- packages/api-core/modules/types/exceptionCodes.type.ts
+++ packages/api-core/modules/types/exceptionCodes.type.ts
@@ -1,3 +1,4 @@
 export enum ExceptionCodes {
   UnverifiedUser = 400003,
+  acceptBBRCommitment = 400004,
 }
