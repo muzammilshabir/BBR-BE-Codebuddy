02faa991a4d0680e7f816bd18b3d7bad64b19aee
Suman Sarkar <suman@codebuddy.co>
2024-08-27 18:34:00 +0530

    :sparkles: feat(api): updated residenceType schema

--- apps/api/src/residenceType/schema/residenceType.schema.ts
+++ apps/api/src/residenceType/schema/residenceType.schema.ts
@@ -1,10 +1,34 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
-import { Document } from 'mongoose';
+import { Document, Types } from 'mongoose';
 
 @Schema({ timestamps: true })
 export class ResidenceType extends Document {
   @Prop({ required: true, unique: true })
   type: string;
+
+  @Prop({
+    type: [
+      {
+        ImageId: { type: Types.ObjectId, ref: 'Upload' },
+        type: { type: String, required: false },
+      },
+    ],
+    _id: false,
+    default: [],
+  })
+  upload?: {
+    ImageId: Types.ObjectId;
+    type?: string;
+  }[];
+
+  @Prop({ type: Types.ObjectId, ref: 'User', required: false })
+  createdBy?: Types.ObjectId;
+
+  @Prop({ type: Types.ObjectId, ref: 'User', required: false })
+  updatedBy?: Types.ObjectId;
+
+  @Prop({ default: false })
+  isDeleted: boolean;
 
   createdAt: Date;
   updatedAt: Date;
