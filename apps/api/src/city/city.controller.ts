2993640b1a188ca63250a90cd327256098a99804
Suman Sarkar <suman@codebuddy.co>
2024-08-29 12:47:29 +0530

    :sparkles: feat(api): list cities api created

--- apps/api/src/city/city.controller.ts
+++ apps/api/src/city/city.controller.ts
@@ -1,9 +1,24 @@
-import { Controller } from '@nestjs/common';
-import { ApiTags } from '@nestjs/swagger';
+import { Controller, Get, Query, UsePipes } from '@nestjs/common';
+import { ApiOperation, ApiTags } from '@nestjs/swagger';
 import { CityService } from './city.service';
+import { Public } from '../auth/decorators/public.decorator';
+import { JoiValidationPipe } from '@bbr/api-core/modules/joi-validation-pipe/joi-validation-pipe.interceptor';
+import { ResponseService } from '@bbr/api-core/modules/response/response.service';
+import { ListCityDto, listCitySchema } from './dto/listCity.dto';
 
 @ApiTags('City')
 @Controller('city')
 export class CityController {
   constructor(private readonly cityService: CityService) {}
+
+  @Get()
+  @ApiOperation({
+    summary: 'List all Cities',
+  })
+  @Public()
+  @UsePipes(new JoiValidationPipe(listCitySchema, 'query'))
+  async list(@Query() listCityDto: ListCityDto) {
+    const data = await this.cityService.findAll(listCityDto);
+    return ResponseService.buildResponse(data);
+  }
 }
