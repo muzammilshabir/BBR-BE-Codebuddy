f6bb551d4a912c94b3b6ee2a70c41a82ca71ab7e
Suman Sarkar <suman@codebuddy.co>
2024-09-03 18:45:06 +0530

    :sparkles: feat(api): added address with valid city and country when creating a residence.

--- apps/api/src/residences/residences.module.ts
+++ apps/api/src/residences/residences.module.ts
@@ -6,10 +6,21 @@ import { ResidenceController } from './residences.controller';
 import { ResidenceRepository } from './residences.repository';
 import { ResidenceSeeder } from './residences.seeder';
 import { ResidencesFixture } from './residences.fixture';
+import { City, CitySchema } from '../city/schema/city.schema';
+import { CityRepository } from '../city/city.repository';
 
 @Module({
-  imports: [MongooseModule.forFeature([{ name: Residence.name, schema: ResidenceSchema }])],
-  providers: [ResidenceService, ResidenceRepository, ResidenceSeeder, ResidencesFixture],
+  imports: [
+    MongooseModule.forFeature([{ name: Residence.name, schema: ResidenceSchema }]),
+    MongooseModule.forFeature([{ name: City.name, schema: CitySchema }]),
+  ],
+  providers: [
+    ResidenceService,
+    ResidenceRepository,
+    ResidenceSeeder,
+    ResidencesFixture,
+    CityRepository,
+  ],
   exports: [ResidenceSeeder, ResidencesFixture, ResidenceService],
   controllers: [ResidenceController],
 })
