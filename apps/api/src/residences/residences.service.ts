282deb8104e9b2884444ca73596e972188b0f8b8
Suman Sarkar <suman@codebuddy.co>
2024-09-10 18:59:14 +0530

    :sparkles: feat(api): created API to list residence draft request with filters

--- apps/api/src/residences/residences.service.ts
+++ apps/api/src/residences/residences.service.ts
@@ -350,7 +350,7 @@ export class ResidenceService {
     return { pagination, residences: transformedResidence };
   }
 
-  private transformResidences(residences: Residence[]): Residence[] {
+  transformResidences(residences: Residence[]): Residence[] {
     return residences.map((residence) => {
       const transformedResidence: any = { ...residence };
 
