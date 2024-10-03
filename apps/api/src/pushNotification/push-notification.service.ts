9af99b1a1690d87fc04b2e958e429fe0c89d4a7e
Haseeb A. Basil <haseeb.basil@gmail.com>
2024-10-03 14:31:06 +0500

    Manage FCM topics

--- apps/api/src/pushNotification/push-notification.service.ts
+++ apps/api/src/pushNotification/push-notification.service.ts
@@ -24,4 +24,18 @@ export class PushNotificationService {
       },
     });
   }
+
+  /**
+   * Add user devices to a FCM Topic. Creates topic if it doesn't already exists.
+   * @param token 
+   * @param topic 
+   * @returns 
+   */
+  async addUserToGroup(token: string | string[], topic: string) {
+    return admin.messaging().subscribeToTopic(token, topic);
+  }
+
+  async removeUserFromGroup(token: string | string[], topic: string) {
+    return admin.messaging().unsubscribeFromTopic(token, topic);
+  }
 }
