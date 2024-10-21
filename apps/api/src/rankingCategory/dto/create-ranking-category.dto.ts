9055fcc05ede83cb67dc486f62c2d9d3980abd07
abhichek <abhisheksoni@codebuddy.co>
2024-10-21 13:52:26 +0530

    :bug: fix(api): add description feild in ranking category

--- apps/api/src/rankingCategory/dto/create-ranking-category.dto.ts
+++ apps/api/src/rankingCategory/dto/create-ranking-category.dto.ts
@@ -38,12 +38,20 @@ export class RankingCriteriaDto {
 
 export class CreateRankingCategoryDto {
   @ApiProperty({
-    description: 'The unique title of the ranking category',
+    description: 'The title of the ranking category',
     example: 'Top 10 Best Cities to Live',
     required: true,
     type: String,
   })
   title: string;
+
+  @ApiProperty({
+    description: 'The description of the ranking category',
+    example: 'This is top 10 Best Cities to Live',
+    required: true,
+    type: String,
+  })
+  description: string;
 
   @ApiProperty({
     description: 'The category type for ranking',
@@ -100,6 +108,7 @@ export class CreateRankingCategoryDto {
 
 export const createRankingCategorySchema = Joi.object({
   title: Joi.string().required().max(100).trim(),
+  description: Joi.string().optional(),
   categoryType: Joi.string()
     .valid(...Object.values(CategoryType))
     .required(),
