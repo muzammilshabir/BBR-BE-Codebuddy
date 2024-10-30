537ab3671ac0ad45581cb9663a4d52fca04013f4
abhichek <abhisheksoni@codebuddy.co>
2024-10-30 17:08:29 +0530

    :bug: fix(api): populate rooms property

--- apps/api/src/rankingRequestDraft/dto/getRankingRequestDrafById.dto.ts
+++ apps/api/src/rankingRequestDraft/dto/getRankingRequestDrafById.dto.ts
@@ -5,7 +5,7 @@ import { joiObjectIdValidator } from '@bbr/api-core/modules/custome-validations/
 export class GetRankingRequestDraftByIdDto {
   @ApiProperty({
     example: '64b1b5f4e05c12a1f5d8e7c2',
-    description: 'ID of the Ranking Request draft',
+    description: 'ID of the Ranking Request',
     required: true,
   })
   id: string;
