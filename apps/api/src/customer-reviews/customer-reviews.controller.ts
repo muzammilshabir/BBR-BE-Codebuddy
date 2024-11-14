e35ff12650af24f6191c2ed369b4df16617177cc
saurav <saurav@codebuddy.co>
2024-11-14 22:33:47 +0530

    :sparkles: feat(api): created review-request api

--- apps/api/src/customer-reviews/customer-reviews.controller.ts
+++ apps/api/src/customer-reviews/customer-reviews.controller.ts
@@ -1,4 +1,31 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post, UsePipes } from '@nestjs/common';
+import { ApiBearerAuth, ApiOperation, ApiTags } from '@nestjs/swagger';
+import { CustomerReviewsService } from './customer-reviews.service';
+import { Roles } from '../auth/decorators/roles.decorator';
+import { UserRole } from '../users/enum/user.enum';
+import { JoiValidationPipe } from '../../../../packages/api-core/modules/joi-validation-pipe/joi-validation-pipe.interceptor';
+import { GetCurrentUser } from '../auth/decorators/getCurrentUser.decorator';
+import { JwtPayloadType } from '../auth/type/jwt-payload.type';
+import { ResponseService } from '../../../../packages/api-core/modules/response/response.service';
+import { RequestReviewDto, requestReviewDtoSchema } from './dto/request-review.dto';
 
+@ApiTags('Customer-Review')
 @Controller('customer-reviews')
-export class CustomerReviewsController {}
+export class CustomerReviewsController {
+  constructor(private readonly customerReviewsService: CustomerReviewsService) {}
+
+  @Post('/request-review')
+  @ApiOperation({
+    summary: 'Request Review from buyer',
+  })
+  @ApiBearerAuth()
+  @Roles(UserRole.SELLER)
+  @UsePipes(new JoiValidationPipe(requestReviewDtoSchema, 'body'))
+  async requestReview(
+    @GetCurrentUser() userFromToken: JwtPayloadType,
+    @Body() requestReviewDto: RequestReviewDto
+  ) {
+    const result = await this.customerReviewsService.requestReview(userFromToken, requestReviewDto);
+    return ResponseService.buildResponse({ result }, 'Review requested successfully');
+  }
+}
