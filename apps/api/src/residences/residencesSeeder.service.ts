952548077a6dfc572c2b4ba07b0bc7e722f54699
abhichek <abhisheksoni@codebuddy.co>
2024-12-19 03:38:34 +0530

    :bug: fix(api): fix bugs

--- apps/api/src/residences/residencesSeeder.service.ts
+++ apps/api/src/residences/residencesSeeder.service.ts
@@ -454,7 +454,7 @@ export class ResidenceSeederService {
               'geographical_area_id',
             ];
             //Todo: check this logic
-            const foundIdField = idFields.find((field) => rankingCategoryTyped[field] != null);
+            const foundIdField = idFields.find((field) => rankingCategoryTyped[field] === 1);
 
             if (!foundIdField) {
               throw new Error(
