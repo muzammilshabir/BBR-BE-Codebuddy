1d0760f471fb1b30e0befcc32e930607d237855f
shivanshu-codebuddy <shivanshu@codebuddy.co>
2025-01-16 16:46:38 +0530

    :sparkles: feat(api): added Quick Replies Feature

---
+++ apps/api/src/quick-replies/schema/quick-replies.schema.ts
@@ -0,0 +1,24 @@
+import {Prop, Schema, SchemaFactory} from '@nestjs/mongoose';
+import { Document, Types } from 'mongoose';
+
+
+@Schema({ timestamps: true})
+
+export class QuickReplies extends Document{
+
+    @Prop({type: Types.ObjectId ,required: true})
+    userId: Types.ObjectId;
+
+    @Prop({ required: true})
+    message: string;
+
+    @Prop({default: true, required: false})
+    active: boolean;
+
+    createdAt: Date;
+    updatedAt: Date;
+
+}
+
+
+export const QuickRepliesSchema = SchemaFactory.createForClass(QuickReplies);
