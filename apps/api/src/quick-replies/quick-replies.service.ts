98a85bec006908aea4c419cd84d03937357bebb5
shivanshu-codebuddy <shivanshu@codebuddy.co>
2025-01-16 17:15:07 +0530

    Index UserId in Quick Replies Schema

--- apps/api/src/quick-replies/quick-replies.service.ts
+++ apps/api/src/quick-replies/quick-replies.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from "@nestjs/common";
 import { InjectModel } from "@nestjs/mongoose";
 import { Model } from "mongoose";
 import { QuickReplies } from "./schema/quick-replies.schema";
-import { CreateQuickReplyDto } from "./dto/create-quick-reply.dto";
+// import { CreateQuickReplyDto } from "./dto/create-quick-reply.dto";
 
 
 
